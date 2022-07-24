bd9a4f624e48cefe209ec51335070eec20121b33
Vignesh E <evicky2002@gmail.com>
2022-07-24 07:22:48 +0530

    added animations

--- app/src/main/java/com/vignesh/attendancetracker/MainActivity.kt
+++ app/src/main/java/com/vignesh/attendancetracker/MainActivity.kt
@@ -3,10 +3,14 @@ package com.vignesh.attendancetracker
 import android.app.ProgressDialog
 import android.content.Context
 import android.content.SharedPreferences
+import android.content.res.ColorStateList
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
 import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
@@ -21,7 +25,7 @@ import java.security.AccessController.getContext
 
 class MainActivity : AppCompatActivity() {
     var progressBar: ProgressBar? = null
-        var f:ProgressView? =null
+    private var tvWait:TextView? = null
     private lateinit var myApplication: GlobalStorage
     private lateinit var sharedPreferences: SharedPreferences
 
@@ -34,9 +38,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         WindowCompat.setDecorFitsSystemWindows(window, false)
         hideSystemBars()
-
+        tvWait = findViewById(R.id.tvWait)
         progressBar = findViewById(R.id.progressBar)
         progressBar?.setVisibility(View.VISIBLE);
+        tvWait?.visibility = View.VISIBLE
 
         bottomNavigationView = findViewById(R.id.bottomNavigationView)
         homeFragment = HomeFragment()
@@ -50,6 +55,7 @@ class MainActivity : AppCompatActivity() {
                 supportFragmentManager.beginTransaction().apply {
                     replace(R.id.flFragment,homeFragment)
                     commit()
+                    tvWait?.visibility = View.INVISIBLE
                     progressBar?.setVisibility(View.GONE);
                 }},2000
         )
@@ -57,12 +63,14 @@ class MainActivity : AppCompatActivity() {
             when(it.itemId){
                 R.id.miHome ->{
                     supportFragmentManager.beginTransaction().apply {
+                        setCustomAnimations(R.anim.enter_from_left, R.anim.exit_to_right, R.anim.enter_from_right, R.anim.exit_to_left)
                         replace(R.id.flFragment,homeFragment)
                         commit()
                     }
                 }
                 R.id.miDashBoard ->{
                     supportFragmentManager.beginTransaction().apply {
+                        setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left, R.anim.enter_from_left, R.anim.exit_to_right)
                         replace(R.id.flFragment,dashboardFragment)
                         commit()
                     }
--- app/src/main/java/com/vignesh/attendancetracker/fragments/DashboardFragment.kt
+++ app/src/main/java/com/vignesh/attendancetracker/fragments/DashboardFragment.kt
@@ -7,9 +7,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.button.MaterialButton
 import com.google.firebase.auth.FirebaseAuth
 import com.vignesh.attendancetracker.GlobalStorage
 import com.vignesh.attendancetracker.LoginActivity
@@ -20,7 +23,7 @@ class DashboardFragment : Fragment(R.layout.fragment_dashboard) {
     private var TAG = "DashboardFragment"
     private var rvDataCard: RecyclerView? = null
     private lateinit var mFirebaseAuth: FirebaseAuth
-    private var tvSignOut: TextView? = null
+    private var btnSignOut: Button? = null
     private var tvTotalPercentage: TextView? = null
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -32,9 +35,9 @@ class DashboardFragment : Fragment(R.layout.fragment_dashboard) {
         rvDataCard?.adapter = subjectDataCardAdapter
         Log.d(TAG,userObject.toString())
         mFirebaseAuth = FirebaseAuth.getInstance()
-        tvSignOut = view.findViewById(R.id.tvSignout)
+        btnSignOut = view.findViewById(R.id.btnSignOut)
         tvTotalPercentage = view.findViewById(R.id.tvTotalPercentage)
-        tvSignOut?.setOnClickListener{
+        btnSignOut?.setOnClickListener{
             mFirebaseAuth.signOut()
             val logout = Intent(view.context,LoginActivity::class.java)
             logout.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
